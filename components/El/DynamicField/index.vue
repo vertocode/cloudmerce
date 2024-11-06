985277fe2b3a82256dd362381c12b9ca5bf478a9
vertocode <evertonvanoni1@gmail.com>
2024-11-06 16:56:14 -0300

    feat: add dynamic field component

---
+++ components/El/DynamicField/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="dynamic-field">
+    <template v-if="field.type === 'text'">
+      <v-text-field
+          :label="field.label"
+          v-model="value"
+          outlined
+      />
+    </template>
+
+    <template v-else-if="field.type === 'number'">
+      <v-text-field
+          :label="field.label"
+          v-model="value"
+          type="number"
+          outlined
+      />
+    </template>
+
+    <template v-else-if="field.type === 'options'">
+      <v-select
+          :label="field.label"
+          :items="field.options || []"
+          v-model="value"
+          outlined
+      />
+    </template>
+
+    <template v-else>
+      <p>Unsupported field type</p>
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+interface Field {
+  label: string;
+  type: 'text' | 'number' | 'options';
+  options?: string[];
+}
+
+const props = defineProps<{ field: Field }>();
+
+const value = ref<string>('')
+</script>
+
+<style scoped>
+.dynamic-field {
+  margin-bottom: 16px;
+}
+</style>
