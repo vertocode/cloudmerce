b1566d4e07ad1cd9275a7a08c3ef68bca0ef5699
vertocode <evertonvanoni1@gmail.com>
2024-12-01 16:04:04 -0300

    feat: type order

--- composables/useOrderById.ts
+++ composables/useOrderById.ts
@@ -1,13 +1,15 @@
+import type { Order } from '~/types/order'
+
 export const useOrderById = (id: string) => {
   const { ecommerceId } = useStoreData()
-  const order = useState(`order-${id}`, () => null)
+  const order = useState<Order | null>(`order-${id}`, () => null)
   const loading = useState<boolean>(`loading-order-${id}`, () => false)
 
   const { get } = useApi()
   const fetchOrder = async () => {
     try {
       loading.value = true
-      order.value = await get(`/order/${id}/${ecommerceId}`)
+      order.value = await get(`/order/${id}/${ecommerceId}`) as Order
     }
     catch (error) {
       console.error(`Erro ao buscar pedido ${id}:`, error)
