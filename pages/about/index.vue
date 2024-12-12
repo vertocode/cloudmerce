2e37ddbdc0d4d4d96c2132b1d8857a835c5ca787
vertocode <evertonvanoni1@gmail.com>
2024-12-12 17:33:21 -0300

    feat: use whitelabel data in about us screen

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,82 +1,68 @@
 <template>
   <div class="about-us">
-    <VContainer class="content-container">
-      <VRow class="platform-section">
-        <VCol
-          cols="12"
-          md="6"
+    <VContainer v-if="whitelabel">
+      <header>
+        <VIcon
+          large
+          class="store-icon"
+          color="var(--primary-color-500)"
         >
-          <h1 class="title">
-            Sobre Nossa Plataforma
-          </h1>
-          <p class="description">
-            Bem-vindo(a) ao nosso sistema de e-commerce! Somos uma plataforma dedicada a oferecer uma experiência de compra segura, moderna e de alta performance.
-            Nossas soluções são desenvolvidas com tecnologia de ponta, garantindo uma navegação fluida e prática. Além disso, prezamos pela segurança e privacidade dos dados,
-            oferecendo suporte constante para que sua jornada de compra seja a melhor possível.
-          </p>
-        </VCol>
-        <VCol
-          cols="12"
-          md="6"
-          class="video-container"
-        >
-          <iframe
-            width="100%"
-            height="315"
-            src="https://www.youtube.com/embed/xNJql5M38N8?si=OrZyVCXIYmvveypT"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
-          />
-        </VCol>
-      </VRow>
+          mdi-store
+        </VIcon>
+        <VImg
+          :src="whitelabel.logoUrl"
+          alt="Logo da Loja"
+          class="store-logo"
+        />
+      </header>
 
-      <VRow class="store-section">
-        <VCol
-          cols="12"
-          md="6"
-        >
-          <VIcon
-            large
-            class="store-icon"
-            color="var(--primary-color-500)"
-          >
-            mdi-store
-          </VIcon>
-          <h2 class="store-title">
-            Sobre a {{ storeName }}
-          </h2>
-          <p class="store-description">
-            {{ storeDescription }}
-          </p>
-        </VCol>
-        <VCol
-          cols="12"
-          md="6"
-        >
-          <VImg
-            :src="storeLogo"
-            alt="Logo da Loja"
-            class="store-image"
-          />
-        </VCol>
-      </VRow>
+      <h2 class="store-title">
+        Sobre a {{ whitelabel.name }}
+      </h2>
+      <p class="store-description">
+        {{ whitelabel.description }}
+      </p>
     </VContainer>
+    <VProgressCircular
+      v-else
+      indeterminate
+      size="100"
+      color="primary"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import { useStoreData } from '~/composables/useStoreData'
+const { getWhitelabel } = useWhitelabel()
 
-const { storeDescription, storeName, storeLogo } = useStoreData()
+const whitelabel = await getWhitelabel()
 </script>
 
 <style scoped lang="scss">
 .about-us {
   padding: 60px 0;
   color: #333;
+
+  header {
+    display: flex;
+    align-items: center;
+    gap: 48px;
+
+    .store-icon {
+      font-size: 48px;
+      margin-bottom: 12px;
+      color: var(--primary-color-600);
+    }
+
+    .store-logo {
+      width: 48px;
+      height: 48px;
+
+      img {
+        width: max-content;
+      }
+    }
+  }
 
   .title, .store-title {
     font-size: 32px;
@@ -92,7 +78,7 @@ const { storeDescription, storeName, storeLogo } = useStoreData()
     margin-bottom: 16px;
   }
 
-  .platform-section, .store-section {
+  .store-section {
     padding: 20px 0;
     display: flex;
     align-items: center;
@@ -105,21 +91,6 @@ const { storeDescription, storeName, storeLogo } = useStoreData()
     max-height: 300px;
     border-radius: 8px;
     box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
-  }
-
-  .store-icon {
-    font-size: 48px;
-    margin-bottom: 12px;
-    color: var(--primary-color-600);
-  }
-
-  .store-image {
-    max-width: 100%;
-    max-height: 200px;
-    border-radius: 8px;
-    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
-    object-fit: contain;
-    margin-top: 20px;
   }
 }
 </style>
