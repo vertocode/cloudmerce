50976350feee8d985e4b6763cb461424f2f9090f
vertocode <evertonvanoni1@gmail.com>
2024-12-23 17:17:21 -0300

    feat: update integration to create order with pix

--- components/Md/Checkout/Steps/components/components/CreditCardForm.vue
+++ components/Md/Checkout/Steps/components/components/CreditCardForm.vue
@@ -3,13 +3,6 @@
     id="form-element"
     @submit="handleSubmit"
   >
-    <p class="mb-5">
-      Não se preocupe, seus dados estão seguros! Utilizamos a tecnologia da <a
-        href="https://docs.stripe.com/security?locale=pt-BR"
-        target="_blank"
-      >Stripe</a> para processar os pagamentos, garantindo a máxima segurança das suas informações. Nenhum dado fornecido aqui será armazenado em nosso sistema.
-    </p>
-
     <div
       v-if="!isFormLoaded"
       class="spinner"
@@ -19,9 +12,6 @@
         size="100"
       />
     </div>
-
-    <div id="link-authentication-element" />
-    <div id="payment-element" />
 
     <div class="action-buttons">
       <VBtn
@@ -48,80 +38,41 @@
 </template>
 
 <script setup lang="ts">
-import type { Stripe } from '@stripe/stripe-js'
-import type { StripeElements } from '@stripe/stripe-js/types/stripe-js/elements-group'
-
 defineProps<{ prev: () => void }>()
 
 const { post } = useApi()
-const { loadStripe } = useClientStripe()
-const { ecommerceId } = useStoreData()
-const { cartId, removeCartId } = useCart()
+const { cartId, removeCartId, total } = useCart()
 const { userData } = useUser()
-const config = useRuntimeConfig()
 const router = useRouter()
 
 const isLoading = ref(false)
 const isFormLoaded = ref(false)
-const messages = ref<string[]>([])
-
-let stripe: Stripe | null = null
-let elements: StripeElements | null = null
-
-onMounted(async () => {
-  const { key: publishableKey } = config.public.stripe || {}
-  if (!publishableKey) {
-    throw new Error('Stripe publishable key not found')
-  }
-  stripe = await loadStripe(publishableKey) as Stripe | null
-
-  const { clientSecret, error: backendError, id } = await $fetch('/api/create-payment-intent')
-
-  if (!id) throw new Error('no id')
-  if (!clientSecret) throw new Error('no client')
-
-  if (backendError) {
-    messages.value.push(backendError as string)
-  }
-  messages.value.push('Client secret returned.')
-
-  if (!stripe) throw new Error('Stripe not loaded')
-
-  elements = stripe.elements({ clientSecret })
-  const paymentElement = elements.create('payment')
-  paymentElement.mount('#payment-element')
-  const linkAuthenticationElement = elements.create('linkAuthentication')
-  linkAuthenticationElement.mount('#link-authentication-element')
-  isLoading.value = false
-  isFormLoaded.value = true
-})
 
 const handleSubmit = async () => {
   try {
-    if (isLoading.value || !stripe || !elements) {
+    if (isLoading.value) {
       return
     }
+
+    const { getWhitelabel } = useWhitelabel()
+
+    const whitelabel = await getWhitelabel()
 
     isLoading.value = true
 
-    const { error, paymentIntent } = await stripe.confirmPayment({
-      elements,
-      confirmParams: {
-      },
-      redirect: 'if_required',
-    })
+    const { name: userName, email: userEmail } = userData.value || {}
 
-    if (error || !paymentIntent) {
-      console.error('Error confirming payment:', error)
-      handleError('Ocorreu um erro ao validar o pagamento, verifique os dados e tente novamente.')
-      isLoading.value = false
-      return
-    }
-
-    const response = await post(`/order/${ecommerceId}`, {
+    const response = await post(`/order/${whitelabel?._id}`, {
       cartId: cartId.value,
       userId: userData.value?._id,
-      paymentIntentId: paymentIntent.id,
+      paymentData: {
+        description: `Compra para ${userName} na loja ${whitelabel?.name}`,
+        paymentMethod: 'pix',
+        totalAmount: total.value,
+        payer: {
+          email: userEmail,
+        },
+      },
     }) as { code: 'success' | 'error', order: { _id: string } }
 
     if (response?.code === 'success') {
