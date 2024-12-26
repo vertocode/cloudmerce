54793564a6f5f2aeb0d565cab99c6db1e2863548
vertocode <evertonvanoni1@gmail.com>
2024-12-26 17:32:13 -0300

    fix: redirect if dont have url registered

--- middleware/01.whitelabel.global.ts
+++ middleware/01.whitelabel.global.ts
@@ -1,8 +1,8 @@
-export default defineNuxtRouteMiddleware(async () => {
+export default defineNuxtRouteMiddleware(async (to) => {
   const { getWhitelabel } = useWhitelabel()
   const whitelabel = await getWhitelabel()
 
-  if (!whitelabel) {
+  if (!whitelabel && to.path !== '/whitelabel-configuration') {
     console.warn('Não foi encontrado uma whitelabel para a loja')
     return navigateTo('/whitelabel-configuration')
   }
